1d45b2bbf0e556c9a181efb090c95e7877d0c52a
Ernesto Canquiz <fdsoil123@gmail.com>
2024-04-01 21:44:51 -0400

    feat: Add vite-pwa plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,12 +24,88 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
     //'@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    "nuxt-lodash",
+    '@pinia/nuxt',    
     '@nuxt/ui',
+    '@vite-pwa/nuxt',
+    "nuxt-lodash",
     '~/modules/User/module'
   ],
   ui: {
     global: true,
+  },
+  pwa: {
+    /* PWA options */
+    
+    
+    
+
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],     
+      manifest: { 
+        name: 'My Awesome PWA',
+        short_name: 'MyPWA',
+        description: 'My Awesome Progressive Web Apps',
+        theme_color: '#ffffff',
+        icons: [{
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        }, {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        }, {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'  
+        }, {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [       
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst' as const,
+            options: {
+              cacheName: 'google-fonts-cache',
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },        
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/ecanquiz/vue-todo-pwa");
+            },
+            //urlPattern: /^https:\/.my-json-server\.typicode\.com/,
+            handler: "NetworkFirst" as const,
+            method: 'GET',
+            options: {
+              cacheName: "api-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true
+      } 
+
+    
+    
+    
   }
 })
